2fe6d2cfa69e911c51d2445e73464f209048dabd
CoryWilk <123584905+CoryWilk@users.noreply.github.com>
2024-07-03 20:40:17 -0500

    Better World Map to start

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -28,8 +28,8 @@ public class Player extends Entity {
     }
 
     public void setDefaultValues() {
-        worldX = gp.tileSize * 22;
-        worldY = gp.tileSize * 45;
+        worldX = gp.tileSize * 23;
+        worldY = gp.tileSize * 21;
         speed = 4;
         direction = "down";
     }
