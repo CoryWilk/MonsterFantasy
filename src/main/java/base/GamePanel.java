4b6789a5a19649f95f48187004b07a05cf2eb3df
CoryWilk <123584905+CoryWilk@users.noreply.github.com>
2024-07-18 19:35:13 -0500

    Added debugging feature to see draw time to ensure optimization when moving further

--- src/main/java/base/GamePanel.java
+++ src/main/java/base/GamePanel.java
@@ -102,6 +102,13 @@ public class GamePanel extends JPanel implements Runnable{
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
 
+        // DEBUG
+        long drawStart = 0;
+        if (keyH.checkDrawTime) {
+            drawStart = System.nanoTime();
+        }
+
+
         // TILE
         tileM.draw(g2);
 
@@ -116,6 +123,15 @@ public class GamePanel extends JPanel implements Runnable{
 
         // UI
         ui.draw(g2);
+
+        // DEBUG
+        if (keyH.checkDrawTime) {
+            long drawEnd = System.nanoTime();
+            long passed = drawEnd - drawStart;
+            g2.setColor(Color.WHITE);
+            g2.drawString("Draw Time: " + passed, 10, 400);
+            System.out.println("Draw Time: " + passed);
+        }
 
         g2.dispose();
     }
