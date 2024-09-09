901479bac7c9f58ecae2a09b7867fc07c562ff1b
CoryWilk <123584905+CoryWilk@users.noreply.github.com>
2024-09-08 20:42:10 -0500

    Organized some variables and added player attacking animation

--- src/main/java/entity/Entity.java
+++ src/main/java/entity/Entity.java
@@ -11,29 +11,35 @@ import java.util.Objects;
 
 public class Entity {
     GamePanel gp;
-    public int worldX;
-    public int worldY;
-    public int speed;
+
     public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
-    public String direction = "down";
+    public BufferedImage attackUp1, attackUp2, attackDown1, attackDown2, attackLeft1,
+                        attackLeft2, attackRight1, attackRight2;
 
-    public int spriteCounter = 0;
-    public int spriteNum = 1;
-
+    public BufferedImage image, image2, image3;
     public Rectangle solidArea = new Rectangle(0, 0, 48, 48);
     public int solidAreaDefaultX, solidAreaDefaultY;
     public boolean collisionOn = false;
-    public int actionLockCounter = 0;
-    public boolean invincible = false;
-    public int invincibleCounter = 0;
     String[] dialogues = new String[20];
-    int dialogueIndex = 0;
-    public BufferedImage image, image2, image3;
-    public String name;
-    public boolean collision = false;
-    public int type; // 0 = PLAYER, 1 = NPC, 2 = MONSTER
 
-    // CHARACTER STATUS
+    // STATE
+    public int worldX, worldY;
+    public String direction = "down";
+    public int spriteNum = 1;
+    int dialogueIndex = 0;
+    public boolean collision = false;
+    public boolean invincible = false;
+    boolean attacking = false;
+
+    // COUNTER
+    public int spriteCounter = 0;
+    public int actionLockCounter = 0;
+    public int invincibleCounter = 0;
+
+    // CHARACTER ATTRIBUTES
+    public int type; // 0 = PLAYER, 1 = NPC, 2 = MONSTER
+    public String name;
+    public int speed;
     public int maxLife;
     public int life;
 
@@ -163,13 +169,13 @@ public class Entity {
     }
 
 
-    public BufferedImage setup(String imagePath) {
+    public BufferedImage setup(String imagePath, int width, int height) {
         UtilityTool uTool = new UtilityTool();
         BufferedImage image = null;
 
         try {
             image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(imagePath + ".png")));
-            image = uTool.scaleImage(image, gp.tileSize, gp.tileSize);
+            image = uTool.scaleImage(image, width, height);
 
         }catch(IOException e){
             e.printStackTrace();
